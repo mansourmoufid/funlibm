0b43bd9a5320f3288cfe5a2c100055cca13fd2d6
Mansour Moufid <mansourmoufid@gmail.com>
2025-09-14 18:26:33 -0400

    Add more constants.

--- constants.h
+++ constants.h
@@ -6,9 +6,13 @@
 static const float pi_float = 3.1415927410125732421875f ;
 static const float pi_hi_float = 3.141592502593994140625f ;
 static const float pi_lo_float = 1.509958025280866422690451145172119140625e-7f ;
+static const float pi_lo1_float = 1.509957883172319270670413970947265625e-7f ;
+static const float pi_lo2_float = 1.07806059069484767853897011491426383145153522491455e-14f ;
 static const double pi_double = 3.141592653589793115997963468544185161590576171875 ;
 static const double pi_hi_double = 3.141592653589793115997963468544185161590576171875 ;
 static const double pi_lo_double = 1.2246467991473532071737640294583966046256921246776e-16 ;
+static const double pi_lo1_double = 1.2246467991473529606547311478922074134605154738069e-16 ;
+static const double pi_lo2_double = 2.16571334784382806216850328780792564845368713263767e-32 ;
 
 #define pi(T) \
     _Generic((T){0}, \
@@ -25,14 +29,28 @@ static const double pi_lo_double = 1.2246467991473532071737640294583966046256921
         float: pi_lo_float, \
         double: pi_lo_double \
     )
+#define pi_lo1(T) \
+    _Generic((T){0}, \
+        float: pi_lo1_float, \
+        double: pi_lo1_double \
+    )
+#define pi_lo2(T) \
+    _Generic((T){0}, \
+        float: pi_lo2_float, \
+        double: pi_lo2_double \
+    )
 
 // 2π
 static const float twopi_float = 6.283185482025146484375f ;
 static const float twopi_hi_float = 6.28318500518798828125f ;
 static const float twopi_lo_float = 3.01991605056173284538090229034423828125e-7f ;
+static const float twopi_lo1_float = 3.01991576634463854134082794189453125e-7f ;
+static const float twopi_lo2_float = 2.1561211813896953570779402298285276629030704498291e-14f ;
 static const double twopi_double = 6.28318530717958623199592693708837032318115234375 ;
 static const double twopi_hi_double = 6.28318530717958623199592693708837032318115234375 ;
 static const double twopi_lo_double = 2.4492935982947064143475280589167932092513842493552e-16 ;
+static const double twopi_lo1_double = 2.4492935982947059213094622957844148269210309476138e-16 ;
+static const double twopi_lo2_double = 4.3314266956876561243370065756158512969073742652753e-32 ;
 
 #define twopi(T) \
     _Generic((T){0}, \
@@ -49,14 +67,28 @@ static const double twopi_lo_double = 2.4492935982947064143475280589167932092513
         float: twopi_lo_float, \
         double: twopi_lo_double \
     )
+#define twopi_lo1(T) \
+    _Generic((T){0}, \
+        float: twopi_lo1_float, \
+        double: twopi_lo1_double \
+    )
+#define twopi_lo2(T) \
+    _Generic((T){0}, \
+        float: twopi_lo2_float, \
+        double: twopi_lo2_double \
+    )
 
 // π∕2
 static const float pi_2_float = 1.57079637050628662109375f ;
 static const float pi_2_hi_float = 1.5707962512969970703125f ;
 static const float pi_2_lo_float = 7.549790126404332113452255725860595703125e-8f ;
+static const float pi_2_lo1_float = 7.549789415861596353352069854736328125e-8f ;
+static const float pi_2_lo2_float = 5.3903029534742383926948505745713191572576761245728e-15f ;
 static const double pi_2_double = 1.5707963267948965579989817342720925807952880859375 ;
 static const double pi_2_hi_double = 1.5707963267948965579989817342720925807952880859375 ;
 static const double pi_2_lo_double = 6.123233995736766035868820147291983023128460623388e-17 ;
+static const double pi_2_lo1_double = 6.1232339957367648032736557394610370673025773690344e-17 ;
+static const double pi_2_lo2_double = 1.08285667392191403108425164390396282422684356631883e-32 ;
 
 #define pi_2(T) \
     _Generic((T){0}, \
@@ -73,6 +105,130 @@ static const double pi_2_lo_double = 6.12323399573676603586882014729198302312846
         float: pi_2_lo_float, \
         double: pi_2_lo_double \
     )
+#define pi_2_lo1(T) \
+    _Generic((T){0}, \
+        float: pi_2_lo1_float, \
+        double: pi_2_lo1_double \
+    )
+#define pi_2_lo2(T) \
+    _Generic((T){0}, \
+        float: pi_2_lo2_float, \
+        double: pi_2_lo2_double \
+    )
+
+// 1∕π
+static const float inv_pi_float = 0.3183098733425140380859375f ;
+static const float inv_pi_hi_float = 0.3183098733425140380859375f ;
+static const float inv_pi_lo_float = 1.284127648659705300815403461456298828125e-8f ;
+static const float inv_pi_lo1_float = 1.284127648659705300815403461456298828125e-8f ;
+static const float inv_pi_lo2_float = 1.46854773981577749284582745659122338111046701669693e-16f ;
+static const double inv_pi_double = 0.31830988618379069121644420192751567810773849487305 ;
+static const double inv_pi_hi_double = 0.31830988618379063570529297066968865692615509033203 ;
+static const double inv_pi_lo_double = 3.583247455607534113928639249044920144085258958814e-17 ;
+static const double inv_pi_lo1_double = 3.5832474556075334976310570451294471661723173316373e-17 ;
+static const double inv_pi_lo2_double = 5.090832193749854671564015787231669243148620866645e-33 ;
+
+#define inv_pi(T) \
+    _Generic((T){0}, \
+        float: inv_pi_float, \
+        double: inv_pi_double \
+    )
+#define inv_pi_hi(T) \
+    _Generic((T){0}, \
+        float: inv_pi_hi_float, \
+        double: inv_pi_hi_double \
+    )
+#define inv_pi_lo(T) \
+    _Generic((T){0}, \
+        float: inv_pi_lo_float, \
+        double: inv_pi_lo_double \
+    )
+#define inv_pi_lo1(T) \
+    _Generic((T){0}, \
+        float: inv_pi_lo1_float, \
+        double: inv_pi_lo1_double \
+    )
+#define inv_pi_lo2(T) \
+    _Generic((T){0}, \
+        float: inv_pi_lo2_float, \
+        double: inv_pi_lo2_double \
+    )
+
+// 1∕(2π)
+static const float inv_2pi_float = 0.15915493667125701904296875f ;
+static const float inv_2pi_hi_float = 0.15915493667125701904296875f ;
+static const float inv_2pi_lo_float = 6.420638243298526504077017307281494140625e-9f ;
+static const float inv_2pi_lo1_float = 6.420638243298526504077017307281494140625e-9f ;
+static const float inv_2pi_lo2_float = 7.3427386990788874642291372829561169055523350834846e-17f ;
+static const double inv_2pi_double = 0.159154943091895345608222100963757839053869247436523 ;
+static const double inv_2pi_hi_double = 0.159154943091895317852646485334844328463077545166016 ;
+static const double inv_2pi_lo_double = 1.791623727803767056964319624522460072042629479407e-17 ;
+static const double inv_2pi_lo1_double = 1.79162372780376674881552852256472358308615866581866e-17 ;
+static const double inv_2pi_lo2_double = 2.5454160968749273357820078936158346215743104333225e-33 ;
+
+#define inv_2pi(T) \
+    _Generic((T){0}, \
+        float: inv_2pi_float, \
+        double: inv_2pi_double \
+    )
+#define inv_2pi_hi(T) \
+    _Generic((T){0}, \
+        float: inv_2pi_hi_float, \
+        double: inv_2pi_hi_double \
+    )
+#define inv_2pi_lo(T) \
+    _Generic((T){0}, \
+        float: inv_2pi_lo_float, \
+        double: inv_2pi_lo_double \
+    )
+#define inv_2pi_lo1(T) \
+    _Generic((T){0}, \
+        float: inv_2pi_lo1_float, \
+        double: inv_2pi_lo1_double \
+    )
+#define inv_2pi_lo2(T) \
+    _Generic((T){0}, \
+        float: inv_2pi_lo2_float, \
+        double: inv_2pi_lo2_double \
+    )
+
+// 2∕π
+static const float inv_pi_2_float = 0.636619746685028076171875f ;
+static const float inv_pi_2_hi_float = 0.636619746685028076171875f ;
+static const float inv_pi_2_lo_float = 2.56825529731941060163080692291259765625e-8f ;
+static const float inv_pi_2_lo1_float = 2.56825529731941060163080692291259765625e-8f ;
+static const float inv_pi_2_lo2_float = 2.9370954796315549856916549131824467622209340333939e-16f ;
+static const double inv_pi_2_double = 0.6366197723675813824328884038550313562154769897461 ;
+static const double inv_pi_2_hi_double = 0.63661977236758127141058594133937731385231018066406 ;
+static const double inv_pi_2_lo_double = 7.166494911215068227857278498089840288170517917628e-17 ;
+static const double inv_pi_2_lo1_double = 7.1664949112150669952621140902588943323446346632746e-17 ;
+static const double inv_pi_2_lo2_double = 1.018166438749970934312803157446333848629724173329e-32 ;
+
+#define inv_pi_2(T) \
+    _Generic((T){0}, \
+        float: inv_pi_2_float, \
+        double: inv_pi_2_double \
+    )
+#define inv_pi_2_hi(T) \
+    _Generic((T){0}, \
+        float: inv_pi_2_hi_float, \
+        double: inv_pi_2_hi_double \
+    )
+#define inv_pi_2_lo(T) \
+    _Generic((T){0}, \
+        float: inv_pi_2_lo_float, \
+        double: inv_pi_2_lo_double \
+    )
+#define inv_pi_2_lo1(T) \
+    _Generic((T){0}, \
+        float: inv_pi_2_lo1_float, \
+        double: inv_pi_2_lo1_double \
+    )
+#define inv_pi_2_lo2(T) \
+    _Generic((T){0}, \
+        float: inv_pi_2_lo2_float, \
+        double: inv_pi_2_lo2_double \
+    )
 
 // π∕n
 static const float pi_3_float = 1.0471975803375244140625f ;
@@ -85,30 +241,6 @@ static const double pi_4_double = 0.78539816339744827899949086713604629039764404
 static const double pi_6_double = 0.52359877559829892668119555310113355517387390136719 ;
 static const double pi_8_double = 0.39269908169872413949974543356802314519882202148437 ;
 static const double pi_16_double = 0.19634954084936206974987271678401157259941101074219 ;
-
-// 1∕π
-static const float inv_pi_float = 0.3183098733425140380859375f ;
-static const float inv_pi_hi_float = 0.3183098733425140380859375f ;
-static const float inv_pi_lo_float = 1.284127648659705300815403461456298828125e-8f ;
-static const double inv_pi_double = 0.31830988618379069121644420192751567810773849487305 ;
-static const double inv_pi_hi_double = 0.31830988618379063570529297066968865692615509033203 ;
-static const double inv_pi_lo_double = 3.583247455607534113928639249044920144085258958814e-17 ;
-
-// 1∕(2π)
-static const float inv_2pi_float = 0.15915493667125701904296875f ;
-static const float inv_2pi_hi_float = 0.15915493667125701904296875f ;
-static const float inv_2pi_lo_float = 6.420638243298526504077017307281494140625e-9f ;
-static const double inv_2pi_double = 0.159154943091895345608222100963757839053869247436523 ;
-static const double inv_2pi_hi_double = 0.159154943091895317852646485334844328463077545166016 ;
-static const double inv_2pi_lo_double = 1.791623727803767056964319624522460072042629479407e-17 ;
-
-// 2∕π
-static const float inv_pi_2_float = 0.636619746685028076171875f ;
-static const float inv_pi_2_hi_float = 0.636619746685028076171875f ;
-static const float inv_pi_2_lo_float = 2.56825529731941060163080692291259765625e-8f ;
-static const double inv_pi_2_double = 0.6366197723675813824328884038550313562154769897461 ;
-static const double inv_pi_2_hi_double = 0.63661977236758127141058594133937731385231018066406 ;
-static const double inv_pi_2_lo_double = 7.166494911215068227857278498089840288170517917628e-17 ;
 
 // k⋅2π ≤ 2²⁴, k = 2670176
 static const float twopi_2_24_float = 16777211.0f ;
